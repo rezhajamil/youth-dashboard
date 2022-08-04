ed4b9ca4c67feb46a1d373e7d78b928d3fa7053a
Azisten <azistencom@gmail.com>
2022-08-04 16:52:35 +0700

    Add Campaign Filter

--- resources/views/broadcast/campaign/index.blade.php
+++ resources/views/broadcast/campaign/index.blade.php
@@ -5,6 +5,20 @@
         <div class="mt-4">
             <h4 class="text-xl font-bold text-gray-600 align-baseline">Campaign</h4>
             <a href="{{ route('campaign.create') }}" class="inline-block px-4 py-2 my-2 font-bold text-white bg-indigo-600 rounded-md hover:bg-indigo-800"><i class="mr-2 fa-solid fa-plus"></i> Data Campaign Baru</a>
+
+            <div class="flex flex-wrap items-end mb-2 gap-x-4">
+                <input type="text" name="search" id="search" placeholder="Search..." class="px-4 rounded-lg">
+                <div class="flex flex-col">
+                    <span class="font-bold text-gray-600">Berdasarkan</span>
+                    <select name="search_by" id="search_by" class="rounded-lg">
+                        <option value="branch">Branch</option>
+                        <option value="program">Program</option>
+                        <option value="posisi">Posisi</option>
+                        <option value="keterangan">Keterangan</option>
+                    </select>
+                </div>
+            </div>
+
             {{-- <span class="inline-block mt-6 mb-2 text-lg font-semibold text-gray-600">Campaign</span> --}}
             <div class="overflow-hidden bg-white rounded-md shadow w-fit">
                 <table class="text-left border-collapse w-fit">
@@ -14,6 +28,7 @@
                             <th class="p-4 text-sm font-medium text-gray-100 uppercase bg-premier">BRANCH</th>
                             <th class="p-4 text-sm font-medium text-gray-100 uppercase bg-premier">DATE</th>
                             <th class="p-4 text-sm font-medium text-gray-100 uppercase bg-premier">PROGRAM</th>
+                            <th class="p-4 text-sm font-medium text-gray-100 uppercase bg-premier">POSISI</th>
                             <th class="p-4 text-sm font-medium text-gray-100 uppercase bg-premier">CAMPAIGN</th>
                             <th class="p-4 text-sm font-medium text-gray-100 uppercase bg-premier">ACTION</th>
 
@@ -24,10 +39,11 @@
                         @foreach($campaign as $key=>$data)
                         <tr class="transition hover:bg-gray-200/40">
                             <td class="p-4 font-bold text-gray-700">{{ $key+1 }}</td>
-                            <td class="p-4 text-gray-700 uppercase whitespace-nowrap ">{{ $data->branch }}</td>
+                            <td class="p-4 text-gray-700 uppercase whitespace-nowrap branch">{{ $data->branch }}</td>
                             <td class="p-4 text-gray-700 uppercase ">{{ date('d-M-Y',strtotime($data->date)) }}</td>
-                            <td class="p-4 text-gray-700 uppercase ">{{ $data->program }}</td>
-                            <td class="p-4 text-gray-700 ">{!! $data->campain !!}</td>
+                            <td class="p-4 text-gray-700 uppercase program">{{ $data->program }}</td>
+                            <td class="p-4 text-gray-700 uppercase posisi">{{ $data->posisi }}</td>
+                            <td class="p-4 text-gray-700 keterangan">{!! $data->campain !!}</td>
                             <td class="p-4 text-gray-700 uppercase ">
                                 <form action="{{ route('campaign.destroy',$data->id) }}" method="post">
                                     @csrf
@@ -49,7 +65,15 @@
 <script>
     $(document).ready(function() {
         $("#search").on("input", function() {
-            let search = $(this).val();
+            find();
+        });
+
+        $("#search_by").on("input", function() {
+            find();
+        });
+
+        const find = () => {
+            let search = $("#search").val();
             let searchBy = $('#search_by').val();
             let pattern = new RegExp(search, "i");
             $(`.${searchBy}`).each(function() {
@@ -60,8 +84,7 @@
                     $(this).parent().hide();
                 }
             });
-        });
-
+        }
     })
 
 </script>
