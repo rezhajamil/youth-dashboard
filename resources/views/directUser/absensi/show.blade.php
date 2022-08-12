052bed6ca1869125597718c573a9f06ffd6350b3
Azisten <azistencom@gmail.com>
2022-08-12 09:04:42 +0700

    Add Detail Absensi

--- resources/views/directUser/absensi/show.blade.php
+++ resources/views/directUser/absensi/show.blade.php
@@ -3,6 +3,8 @@
 <div class="w-full mx-4 my-4">
     <div class="flex flex-col">
         <div class="mt-4">
+
+            <a href="{{ url()->previous() }}" class="inline-block px-4 py-2 my-2 font-bold text-white bg-indigo-600 rounded-md hover:bg-indigo-800"><i class="mr-2 fa-solid fa-arrow-left"></i> Kembali</a>
             <h4 class="text-xl font-bold text-gray-600 align-baseline">Detail Absensi</h4>
 
             <span class="inline-block mt-6 mb-2 text-lg font-semibold text-gray-600">Absensi : {{ $absensi[0]->nama }} | {{ $absensi[0]->telp }}</span>
@@ -10,44 +12,83 @@
                 <table class="text-left border-collapse w-fit">
                     <thead class="border-b">
                         <tr>
-                            <th class="p-2 text-sm font-bold text-gray-100 uppercase border-2 border-tersier bg-premier">No</th>
-                            <th class="p-2 text-sm font-medium text-center text-gray-100 uppercase border-2 border-tersier bg-premier">Nama</th>
+                            <th class="p-2 text-sm font-medium text-center text-gray-100 uppercase border-2 border-tersier bg-premier">Info</th>
                             @foreach ($period as $data)
                             <th class="p-2 text-sm font-medium text-center text-gray-100 uppercase border-2 whitespace-nowrap border-tersier bg-premier">{{ date('d',strtotime($data)) }}</th>
                             @endforeach
                         </tr>
                     </thead>
                     <tbody>
-                        @php
-                        $telp=''
-                        @endphp
-                        @foreach ($absensi as $key=>$data)
-                        @if ($data->telp!=$telp)
                         <tr class="transition hover:bg-gray-200/40">
-                            <td class="p-2 font-bold text-center text-gray-700 border-2">{{ $key+1 }}</td>
                             <td class="p-2 text-gray-700 uppercase border-2 ">
-                                <a href="{{ route('direct_user.absensi.show',$data->telp) }}" class="transition hover:text-indigo-800">
-                                    <span class="block font-semibold whitespace-nowrap">{{ $data->nama }}</span>
-                                </a>
-                                <a href="{{ route('direct_user.absensi.show',$data->telp) }}" class="transition hover:text-indigo-800">
-                                    <span class="block text-xs font-light">{{ $data->telp }}</span>
-                                </a>
+                                <span class="block font-semibold whitespace-nowrap">Kehadiran</span>
                             </td>
                             @foreach ($period as $item)
                             <td class="p-2 text-center text-gray-700 uppercase border-2 ">
                                 @foreach ($absensi as $absen)
-                                @if (date('Y-m-d',strtotime($item))==$absen->date && $data->telp==$absen->telp)
+                                @if (date('Y-m-d',strtotime($item))==$absen->date)
                                 <i class="text-3xl text-green-600 fa-solid fa-circle-check"></i>
                                 @endif
                                 @endforeach
                             </td>
                             @endforeach
                         </tr>
-                        @endif
-                        @php
-                        $telp=$data->telp
-                        @endphp
-                        @endforeach
+                        <tr class="transition hover:bg-gray-200/40">
+                            <td class="p-2 text-gray-700 uppercase border-2 ">
+                                <span class="block font-semibold whitespace-nowrap">Jarak (Km)</span>
+                            </td>
+                            @foreach ($period as $item)
+                            <td class="p-2 text-center text-gray-700 border-2 ">
+                                @foreach ($absensi as $absen)
+                                @if (date('Y-m-d',strtotime($item))==$absen->date)
+                                <span class="block whitespace-nowrap">{{ number_format($absen->jarak,0,",",".") }}</span>
+                                @endif
+                                @endforeach
+                            </td>
+                            @endforeach
+                        </tr>
+                        <tr class="transition hover:bg-gray-200/40">
+                            <td class="p-2 text-gray-700 uppercase border-2 ">
+                                <span class="block font-semibold whitespace-nowrap">Time In (Wib)</span>
+                            </td>
+                            @foreach ($period as $item)
+                            <td class="p-2 text-center text-gray-700 border-2 ">
+                                @foreach ($absensi as $absen)
+                                @if (date('Y-m-d',strtotime($item))==$absen->date)
+                                <span class="block whitespace-nowrap">{{ date('H:i',strtotime($absen->time_in)) }}</span>
+                                @endif
+                                @endforeach
+                            </td>
+                            @endforeach
+                        </tr>
+                        <tr class="transition hover:bg-gray-200/40">
+                            <td class="p-2 text-gray-700 uppercase border-2 ">
+                                <span class="block font-semibold whitespace-nowrap">Jarak Keluar (Km)</span>
+                            </td>
+                            @foreach ($period as $item)
+                            <td class="p-2 text-center text-gray-700 border-2 ">
+                                @foreach ($absensi as $absen)
+                                @if (date('Y-m-d',strtotime($item))==$absen->date)
+                                <span class="block whitespace-nowrap">{{ number_format($absen->jarak_keluar,0,",",".") }}</span>
+                                @endif
+                                @endforeach
+                            </td>
+                            @endforeach
+                        </tr>
+                        <tr class="transition hover:bg-gray-200/40">
+                            <td class="p-2 text-gray-700 uppercase border-2 ">
+                                <span class="block font-semibold whitespace-nowrap">Time Out (Wib)</span>
+                            </td>
+                            @foreach ($period as $item)
+                            <td class="p-2 text-center text-gray-700 border-2 ">
+                                @foreach ($absensi as $absen)
+                                @if (date('Y-m-d',strtotime($item))==$absen->date)
+                                <span class="block whitespace-nowrap">{{ date('H:i',strtotime($absen->time_out)) }}</span>
+                                @endif
+                                @endforeach
+                            </td>
+                            @endforeach
+                        </tr>
                     </tbody>
                 </table>
             </div>
