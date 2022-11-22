e5a417e2d2674ae35635e5f851e4274d3d430559
Azisten <azistencom@gmail.com>
2022-11-22 08:32:32 +0700

    Fix Broadcast Cluster

--- resources/views/broadcast/index.blade.php
+++ resources/views/broadcast/index.blade.php
@@ -252,7 +252,7 @@
                 </table>
             </div>
 
-            <span class="block mt-6 mb-2 text-lg font-semibold text-gray-600">Broadcast/By Cluster</span>
+            {{-- <span class="block mt-6 mb-2 text-lg font-semibold text-gray-600">Broadcast/By Cluster</span>
             <div class="overflow-hidden bg-white rounded-md shadow w-fit table-broadcast">
                 <table class="text-left border-collapse w-fit">
                     <thead class="border-b">
@@ -262,7 +262,6 @@
                             <th colspan="3" class="p-3 text-sm font-medium text-center text-gray-100 uppercase border-2 border-tersier bg-premier">Whitelist</th>
                             <th colspan="4" class="p-3 text-sm font-medium text-center text-gray-100 uppercase border-2 border-tersier bg-premier">Broadcast</th>
 
-                            {{-- <th class="p-3 text-sm font-medium text-center text-gray-100 uppercase border-2 border-tersier bg-premier">Action</th> --}}
                         </tr>
                         <tr>
                             <th class="p-3 text-sm font-medium text-center text-gray-100 uppercase border-2 border-tersier bg-sekunder">Total</th>
@@ -278,23 +277,22 @@
                         @foreach ($broadcast_cluster as $key=>$data)
                         <tr class="transition hover:bg-gray-200/40">
                             <td class="p-3 font-bold text-gray-700 border-2">{{ $key+1 }}</td>
-                            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->cluster }}</td>
-                            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->wl }}</td>
-                            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->diambil }}</td>
-                            <td class="p-3 text-gray-700 uppercase border-2 whitespace-nowrap">{{ number_format(($data->diambil/$data->wl)*100,2).' %' }}</td>
-                            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->sudah }}</td>
-                            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->belum }}</td>
-                            <td class="p-3 text-gray-700 uppercase border-2 whitespace-nowrap">{{ number_format(($data->sudah/$data->wl)*100,2).' %' }}</td>
-                            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->sisa }}</td>
-                            {{-- <td class="p-3 text-gray-700 border-b"></td> --}}
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->cluster }}</td>
+            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->wl }}</td>
+            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->diambil }}</td>
+            <td class="p-3 text-gray-700 uppercase border-2 whitespace-nowrap">{{ number_format(($data->diambil/$data->wl)*100,2).' %' }}</td>
+            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->sudah }}</td>
+            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->belum }}</td>
+            <td class="p-3 text-gray-700 uppercase border-2 whitespace-nowrap">{{ number_format(($data->sudah/$data->wl)*100,2).' %' }}</td>
+            <td class="p-3 text-gray-700 uppercase border-2 ">{{ $data->sisa }}</td>
+            </tr>
+            @endforeach
+            </tbody>
+            </table>
+        </div> --}}
 
-        </div>
     </div>
+</div>
 
 </div>
 @endsection
