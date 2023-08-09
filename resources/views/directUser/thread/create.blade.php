c0959a8fabe637fe1693f1ea5f8cffd883ea88fe
John Doe <yamatogija@gmail.com>
2023-08-09 14:21:13 +0700

    Fix Thread

--- resources/views/directUser/thread/create.blade.php
+++ resources/views/directUser/thread/create.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.dashboard.app', ['plain' => true])
 @section('body')
     <section class="flex flex-col w-full h-full min-h-screen px-2 py-4 bg-gray-100 font-caregold">
-        <div class="flex items-center justify-center w-full mb-4 gap-x-2">
+        {{-- <div class="flex items-center justify-center w-full mb-4 gap-x-2">
             <img src="{{ asset('images/logo-new.png') }}" alt="Logo Yout" class="h-10">
             <span
                 class="text-2xl font-bold text-transparent bg-gradient-to-br from-y_premier to-y_tersier font-batik bg-clip-text">Threads</span>
-        </div>
+        </div> --}}
+        <a href="{{ route('thread.index', ['telp' => request()->get('telp')]) }}"
+            class="px-3 py-1 text-sm font-semibold text-white transition-all rounded-md w-fit bg-gradient-to-br from-y_tersier to-y_tersier/60 hover:to-y_tersier">
+            <i class="mr-2 text-white fa-solid fa-arrow-left"></i>
+            <span class="">Kembali</span>
+        </a>
         <div class="px-3 py-2 my-6 bg-white rounded-md shadow-xl">
             <form action="{{ route('thread.store') }}" class="" method="POST">
                 @csrf
@@ -16,7 +21,7 @@
                 </div>
                 <input type="hidden" name="telp" value="{{ request()->get('telp') }}">
                 <input type="hidden" name="message" id="message" value="{!! old('message') !!}">
-                <trix-editor input="message" row="20"></trix-editor>
+                <trix-editor input="message" class="h-72"></trix-editor>
                 @error('message')
                     <span class="block text-sm italic text-red-600">{{ $message }}</span>
                 @enderror
