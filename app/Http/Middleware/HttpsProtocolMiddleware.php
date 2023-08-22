4e5f1c0e32a1266d9391709bf8b4e3e82122584d
John Doe <yamatogija@gmail.com>
2023-08-22 14:56:08 +0700

    Fix Https

--- app/Http/Middleware/HttpsProtocolMiddleware.php
+++ app/Http/Middleware/HttpsProtocolMiddleware.php
@@ -17,6 +17,7 @@ class HttpsProtocolMiddleware
     public function handle(Request $request, Closure $next)
     {
         if (!$request->secure() && app()->environment('production')) {
+            ddd($request->getRequestUri());
             return redirect()->secure($request->getRequestUri());
         }
 
