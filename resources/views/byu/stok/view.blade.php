70f4577e09b7ee28e1848bc6e0643000fe79d23f
Rezha Jamil Nasution <rezha@Rezhas-MacBook-Pro.local>
2023-10-27 16:07:49 +0700

    Update Byu Stok

--- resources/views/byu/stok/view.blade.php
+++ resources/views/byu/stok/view.blade.php
@@ -5,28 +5,33 @@
             <div class="mt-4">
                 <h4 class="text-xl font-bold text-gray-600 align-baseline">Hasil Input Stok ByU Legacy</h4>
 
-                <div class="flex justify-between mt-4 ">
-                    <form class="flex flex-wrap items-center gap-x-4 gap-y-2" action="{{ route('byu.stok.view') }}"
+                <div class="flex justify-between mt-4 flex-nowrap">
+                    <form class="flex items-center flex-nowrap gap-x-4 gap-y-2" action="{{ route('byu.stok.view') }}"
                         method="get">
                         <input type="date" name="start_date" id="start_date" class="px-4 rounded-lg"
                             value="{{ request()->get('start_date') }}" required>
                         <span class="">s/d</span>
                         <input type="date" name="end_date" id="end_date" class="px-4 rounded-lg"
                             value="{{ request()->get('end_date') }}" required>
-                        <div class="flex gap-x-3">
+                        <div class="flex items-center gap-x-3">
                             <button
-                                class="px-4 py-2 font-bold text-white transition rounded-lg bg-y_premier hover:bg-y_premier"><i
+                                class="px-4 py-2 font-bold text-white transition rounded-lg h-fit whitespace-nowrap bg-y_premier hover:bg-y_premier"><i
                                     class="mr-2 fa-solid fa-magnifying-glass"></i>Cari</button>
                             @if (request()->get('start_date'))
                                 <a href="{{ route('byu.stok.view') }}"
-                                    class="px-4 py-2 font-bold text-white transition bg-gray-600 rounded-lg hover:bg-gray-800"><i
-                                        class="mr-2 fa-solid fa-circle-xmark"></i>Reset</a>
+                                    class="px-4 py-2 font-bold text-white transition bg-gray-600 rounded-lg h-fit whitespace-nowrap hover:bg-gray-800"><i
+                                        class="mr-2 fa-solid fa-circle-xmark"></i>Reset
+                                </a>
+                                <button id="btn-excel"
+                                    class="inline-block px-4 py-2 my-2 font-semibold text-white transition-all bg-teal-600 rounded-md h-fit whitespace-nowrap hover:bg-teal-800">
+                                    <i class="mr-2 fa-solid fa-file-arrow-down"></i>Excel
+                                </button>
                             @endif
                         </div>
                     </form>
                 </div>
 
-                <div class="my-4 overflow-hidden bg-white rounded-md shadow w-fit" id="table-region">
+                <div class="my-4 overflow-hidden bg-white rounded-md shadow w-fit" id="table-container">
                     <table class="text-left border-collapse w-fit">
                         <thead class="border-b">
                             <tr>
@@ -58,6 +63,7 @@
     </div>
 @endsection
 @section('script')
+    <script type="text/javascript" src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"></script>
     <script>
         $(document).ready(function() {
             $("#search").on("input", function() {
@@ -90,6 +96,40 @@
                 $("#table-cluster").toggle();
             })
 
+            $("#btn-excel").click(function() {
+                exportTableToExcel('table-container', `Hasil Input Stok ByU Legacy`);
+            });
+
+            function exportTableToExcel(tableID, filename = '') {
+                var downloadLink;
+                var dataType = 'application/vnd.ms-excel';
+                var tableSelect = document.getElementById(tableID);
+                var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+
+                // Specify file name
+                filename = filename ? filename + '.xlsx' : 'excel_data.xlsx';
+
+                // Create download link element
+                downloadLink = document.createElement("a");
+
+                document.body.appendChild(downloadLink);
+
+                if (navigator.msSaveOrOpenBlob) {
+                    var blob = new Blob(['\ufeff', tableHTML], {
+                        type: dataType
+                    });
+                    navigator.msSaveOrOpenBlob(blob, filename);
+                } else {
+                    // Create a link to the file
+                    downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+
+                    // Setting the file name
+                    downloadLink.download = filename;
+
+                    //triggering the function
+                    downloadLink.click();
+                }
+            }
         })
     </script>
 @endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,7 +12,7 @@
 
     <title>DASHBOARD YOUTH JAWARA</title>
 
-    <link rel="icon" href="{{ asset("images/logo-shape-text.png") }}">
+    <link rel="icon" href="{{ asset('images/logo-shape-text.png') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     {{-- <link rel="icon" href="{{ asset('images/mosque.svg') }}"> --}}
@@ -21,12 +22,15 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
     <script src="https://kit.fontawesome.com/b2ba1193ce.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div class="flex flex-col items-center justify-center h-screen px-6 bg-gray-200">
-        <img src="{{ asset('images/logo-new-full.png') }}" alt="" class="h-40 mb-4 aspect-auto wow fadeInDown" data-wow-duration="2s">
-        <div class="w-1/2 max-w-sm p-6 bg-white rounded-md shadow-md">
+        <img src="{{ asset('images/logo-new-full.png') }}" alt="" class="h-40 mb-4 aspect-auto wow fadeInDown"
+            data-wow-duration="2s">
+        <div class="w-3/4 max-w-sm p-6 bg-white rounded-md shadow-md sm:w-1/2">
             <div class="flex items-center justify-center">
                 <span class="text-2xl font-semibold text-gray-700">Login Dashboard</span>
             </div>
@@ -35,18 +39,20 @@
                 @csrf
                 <label class="block">
                     <span class="text-sm text-gray-700">Username</span>
-                    <input type="text" class="block w-full mt-1 rounded-md form-input focus:border-y_premier" name="username">
+                    <input type="text" class="block w-full mt-1 rounded-md form-input focus:border-y_premier"
+                        name="username">
                 </label>
                 @error('username')
-                <span class="text-sm italic text-premier">{{ $message }}</span>
+                    <span class="text-sm italic text-premier">{{ $message }}</span>
                 @enderror
 
                 <label class="block mt-3">
                     <span class="text-sm text-gray-700">Password</span>
-                    <input type="password" class="block w-full mt-1 rounded-md form-input focus:border-y_premier" name="password">
+                    <input type="password" class="block w-full mt-1 rounded-md form-input focus:border-y_premier"
+                        name="password">
                 </label>
                 @error('password')
-                <span class="text-sm italic text-premier">{{ $message }}</span>
+                    <span class="text-sm italic text-premier">{{ $message }}</span>
                 @enderror
 
                 <div class="flex items-center justify-between mt-4">
@@ -60,23 +66,25 @@
                     {{-- <div>
                         <a href="{{ route('password.request') }}" class="block text-sm text-sky-700 fontme hover:underline" href="#">Forgot your password?</a>
                 </div> --}}
-        </div>
+                </div>
 
-        <div class="mt-6">
-            <button class="w-full px-4 py-2 text-sm text-center text-white transition-all rounded-md bg-y_premier hover:bg-y_sekunder">
-                Sign in
-            </button>
+                <div class="mt-6">
+                    <button
+                        class="w-full px-4 py-2 text-sm text-center text-white transition-all rounded-md bg-y_premier hover:bg-y_sekunder">
+                        Sign in
+                    </button>
+                </div>
+                <a href="{{ route('register') }}"
+                    class="inline-block w-full mt-2 text-center underline transition-all text-y_premier hover:text-sky-900">Daftar</a>
+            </form>
         </div>
-        <a href="{{route('register')}}" class="inline-block w-full mt-2 text-center underline transition-all text-y_premier hover:text-sky-900">Daftar</a>
-        </form>
-    </div>
     </div>
 
     <script>
         $(document).ready(function() {
             new WOW().init();
         });
-
     </script>
 </body>
+
 </html>
