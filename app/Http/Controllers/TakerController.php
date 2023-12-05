b95e0a1213891995c4398638c08c6024dd5e0cd8
Rezha Jamil Nasution <rezha@Rezhas-MacBook-Pro.local>
2023-12-05 16:43:04 +0700

    update survey

--- app/Http/Controllers/TakerController.php
+++ app/Http/Controllers/TakerController.php
@@ -49,7 +49,7 @@ class TakerController extends Controller
     {
         ini_set("post_max_size", 0);
         // $data = json_decode(html_entity_decode(stripslashes($request->row)));
-        return response()->json($request);
+        return response()->json($request->data);
         $data = json_decode($request->data, true);
         $res = [];
 
