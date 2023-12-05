52055be9cab47546d230fdd64afd87edba5c7bc3
Rezha Jamil Nasution <rezha@Rezhas-MacBook-Pro.local>
2023-12-05 14:21:43 +0700

    update survey

--- resources/views/sekolah/index.blade.php
+++ resources/views/sekolah/index.blade.php
@@ -150,10 +150,10 @@
                                         <td class="p-2 text-gray-700 border">
                                             <a href="{{ route('sekolah.show', $data->NPSN) }}" target="_blank"
                                                 class="block my-1 text-base font-semibold text-teal-600 transition hover:text-teal-800">Detail</a>
-                                            @if (Auth::user()->privilege != 'cluster')
-                                                <a href="{{ route('sekolah.edit', $data->NPSN) }}" target="_blank"
-                                                    class="block my-1 text-base font-semibold transition text-y_premier hover:text-indigo-800">Edit</a>
-                                            @endif
+                                            {{-- @if (Auth::user()->privilege != 'cluster') --}}
+                                            <a href="{{ route('sekolah.edit', $data->NPSN) }}" target="_blank"
+                                                class="block my-1 text-base font-semibold transition text-y_premier hover:text-indigo-800">Edit</a>
+                                            {{-- @endif --}}
                                             @if ($data->LATITUDE && $data->LONGITUDE)
                                                 <a target="_blank"
                                                     href="http://maps.google.com/maps?z=12&t=m&q=loc:{{ $data->LATITUDE }}+{{ $data->LONGITUDE }}"
