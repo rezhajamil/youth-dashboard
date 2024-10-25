db799c83c18e5de26b5e6d1f7e6f0c5dd3cf38c0
Rezha Jamil <rezha.jn@gmail.com>
2024-10-25 14:47:40 +0700

    update user buddies table

--- resources/views/sales/trade/index.blade.php
+++ resources/views/sales/trade/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.dashboard.app')
 @section('body')
-    <div class="mx-4 w-full">
+    <div class="w-full mx-4">
         <div class="flex flex-col">
             <div class="mt-4">
-                <h4 class="align-baseline text-xl font-bold text-gray-600">Report Trade In Buddies</h4>
+                <h4 class="text-xl font-bold text-gray-600 align-baseline">Report Trade In Buddies</h4>
 
-                <div class="mb-2 flex flex-wrap items-end gap-x-4">
-                    <input type="text" name="search" id="search" placeholder="Search..." class="rounded-lg px-4">
+                <div class="flex flex-wrap items-end mb-2 gap-x-4">
+                    <input type="text" name="search" id="search" placeholder="Search..." class="px-4 rounded-lg">
                     <div class="flex flex-col">
                         <span class="font-bold text-gray-600">Berdasarkan</span>
                         <select name="search_by" id="search_by" class="rounded-lg">
@@ -15,61 +15,66 @@
                             <option value="cluster">Cluster</option>
                             <option value="nama">Nama</option>
                             <option value="user_id">User ID</option>
-                            <option value="user_id">User ID</option>
                             <option value="msisdn">Msisdn</option>
                         </select>
                     </div>
                 </div>
 
                 <button id="btn-excel"
-                    class="my-2 inline-block rounded-md bg-teal-600 px-4 py-2 font-semibold text-white transition-all hover:bg-teal-800">
-                    <i class="fa-solid fa-file-arrow-down mr-2"></i>Excel
+                    class="inline-block px-4 py-2 my-2 font-semibold text-white transition-all bg-teal-600 rounded-md hover:bg-teal-800">
+                    <i class="mr-2 fa-solid fa-file-arrow-down"></i>Excel
                 </button>
 
-                <div class="w-fit overflow-auto rounded-md bg-white shadow" id="table-container">
-                    <table class="w-fit border-collapse overflow-auto text-left">
+                <div class="overflow-auto bg-white rounded-md shadow w-fit" id="table-container">
+                    <table class="overflow-auto text-left border-collapse w-fit">
                         <thead class="border-b">
                             <tr>
-                                <th class="bg-y_tersier p-3 text-sm font-bold uppercase text-gray-100">No</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">Regional</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">Branch</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">Cluster</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">City</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">Nama</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">User ID</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">Agent ID</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">MSISDN</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">Kompetitor</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">Paket</th>
-                                <th class="bg-y_tersier p-3 text-sm font-medium uppercase text-gray-100">Tanggal</th>
+                                <th class="p-3 text-sm font-bold text-gray-100 uppercase bg-y_tersier">No</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Regional</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Branch</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Cluster</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">City</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Nama</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">User ID</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Role</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">TAP</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Agent ID</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">MSISDN</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Kompetitor</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Paket</th>
+                                <th class="p-3 text-sm font-medium text-gray-100 uppercase bg-y_tersier">Tanggal</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($data as $key => $d)
                                 <tr class="hover:bg-gray-200">
-                                    <td class="whitespace-nowrap border-b p-3 font-bold text-gray-700">{{ $key + 1 }}
+                                    <td class="p-3 font-bold text-gray-700 border-b whitespace-nowrap">{{ $key + 1 }}
                                     </td>
-                                    <td class="regional whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b regional whitespace-nowrap">
                                         {{ $d->regional }}</td>
-                                    <td class="branch whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b branch whitespace-nowrap">
                                         {{ $d->branch }}</td>
-                                    <td class="cluster whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b cluster whitespace-nowrap">
                                         {{ $d->cluster }}</td>
-                                    <td class="city whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b city whitespace-nowrap">
                                         {{ $d->city }}</td>
-                                    <td class="nama whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b nama whitespace-nowrap">
                                         {{ $d->user_name }}</td>
-                                    <td class="user_id whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b user_id whitespace-nowrap">
                                         {{ $d->user_id }}</td>
-                                    <td class="agent_id whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b role whitespace-nowrap">
+                                    </td>
+                                    <td class="p-3 text-gray-700 uppercase border-b tap whitespace-nowrap">
+                                    </td>
+                                    <td class="p-3 text-gray-700 uppercase border-b agent_id whitespace-nowrap">
                                         {{ $d->agent_id }}</td>
-                                    <td class="msisdn whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b msisdn whitespace-nowrap">
                                         {{ $d->msisdn }}</td>
-                                    <td class="kompetitor whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b kompetitor whitespace-nowrap">
                                         {{ $d->kompetitor }}</td>
-                                    <td class="paket whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b paket whitespace-nowrap">
                                         {{ $d->paket }}</td>
-                                    <td class="date whitespace-nowrap border-b p-3 uppercase text-gray-700">
+                                    <td class="p-3 text-gray-700 uppercase border-b date whitespace-nowrap">
                                         {{ $d->date }}</td>
                                 </tr>
                             @endforeach
